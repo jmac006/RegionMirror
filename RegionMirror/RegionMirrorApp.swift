b4e558aaf1aa5b2f403a7df69cc468ba0e598e8d
Justin Mac <jmac006@ucr.edu>
2025-09-03 19:50:52 -0700

    Initial Commit

---
+++ RegionMirror/RegionMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegionMirrorApp.swift
+//  RegionMirror
+//
+//  Created by Justin Mac on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RegionMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RegionMirrorTests/RegionMirrorTests.swift
@@ -0,0 +1,17 @@
+//
+//  RegionMirrorTests.swift
+//  RegionMirrorTests
+//
+//  Created by Justin Mac on 9/3/25.
+//
+
+import Testing
+@testable import RegionMirror
+
+struct RegionMirrorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
